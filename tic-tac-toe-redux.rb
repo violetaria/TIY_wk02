3a084ad3a2d5d1e14aa9a264ae1537e3867f4e47
violetaria <terri.chu@outlook.com>
2015-09-28 18:42:19 -0400

    updating winning_boards constant

--- tic-tac-toe-redux.rb
+++ tic-tac-toe-redux.rb
@@ -3,7 +3,7 @@ require 'pry'
 COMPUTER_NAME = "Unimatrix Zero"
 PLAYER1_MARKER = "X"
 PLAYER2_MARKER = "O"
-WINNING_BOARDS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+WINNING_BOARDS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 CENTER = 5
 CORNERS =[1,3,7,9]
 EDGES = [2,4,6,8]
