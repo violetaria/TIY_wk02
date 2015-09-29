ffbabc5f29316cb3d746d0debe4d7822241d5a44
violetaria <terri.chu@outlook.com>
2015-09-29 16:10:29 -0400

    small modification for Normal Mode

--- guessing_game/smart.rb
+++ guessing_game/smart.rb
@@ -1,20 +1,20 @@
 class SmartPlayer
   def initialize
-    @guess = 50
     @low = 1
-    @high = 100
+    @high = MAX_NUMBER
+    @guess = get_middle
   end
 
   def get_guess(last_result)
     if last_result == :high
       @high = @guess-1
-      @guess = (@low + @high) / 2
     elsif last_result == :low
       @low = @guess+1
-      @guess = (@low + @high) / 2
-    else
-      @guess
     end
+    @guess = get_middle
   end
 
+  def get_middle
+    (@low + @high) / 2
+  end
 end
