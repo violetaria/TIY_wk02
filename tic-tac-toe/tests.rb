6d7aa6d1f82b9b669072af8236076ad709264a5d
violetaria <terri.chu@outlook.com>
2015-10-01 15:15:35 -0400

    initial tic tac toe outline + first test file

---
+++ tic-tac-toe/tests.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+
+class PlayerTest < MiniTest::Test
+  def test_can_make_player
+    named_player = Player.new("Terri")
+    assert named_player.name == "Terri"
+
+    unnamed_player = Player.new
+    assert unnamed_player.name == "Unimatrix Zero"
+  end
+end
